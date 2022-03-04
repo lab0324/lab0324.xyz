862d019ace18db2f1d5c1c7398dcb6bd2584b3ed
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2022-03-03 22:05:04 -0500

    updates

--- typeeffect.js
+++ typeeffect.js
@@ -1,11 +1,35 @@
-  var typed = new Typed(".typed", {
-  strings: ["product studio", "microfund", "creative workshop",  "space for learning", "container for multidisciplinary experiments"],
-  typeSpeed: 30,
-  backSpeed: 20,
-  backDelay: 800,
-  startDelay: 500,
-  loop: false,
-  showCursor: true,
-  cursorChar: "_",
-  attr: null,
+var line1 = new Typed(".typed-line1", {
+    strings: ["> lab0324 is a web3 product studio^1000 <br/>> currently building <a href=\"https://www.aikido.work\">www.aikido.work</a>^1000 <br/>> follow us on <a href=\"https://www.twitter.com/lab0324\">twitter</a>"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 0,
+    startDelay: 0,
+    loop: false,
+    showCursor: true,
+    cursorChar: "_",
+    attr: null,
 });
+
+// var line2 = new Typed(".typed-line2", {
+//     strings: ["> currently building <a href=\"https://www.aikido.work\">www.aikido.work</a>"],
+//     typeSpeed: 50,
+//     backSpeed: 20,
+//     backDelay: 800,
+//     startDelay: 7000,
+//     loop: false,
+//     showCursor: false,
+//     cursorChar: "_",
+//     attr: null,
+// });
+
+// var line3 = new Typed(".typed-line3", {
+//     strings: ["> follow us on <a href=\"https://www.twitter.com/lab0324\">twitter</a>"],
+//     typeSpeed: 50,
+//     backSpeed: 20,
+//     backDelay: 800,
+//     startDelay: 13000,
+//     loop: false,
+//     showCursor: false,
+//     cursorChar: "_",
+//     attr: null,
+// });
