73a8dadd7af409145fc8f2f79f33759c9ff2d108
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2022-05-16 16:07:45 -0400

    new homepage

--- typeeffect.js
+++ typeeffect.js
@@ -1,7 +1,7 @@
 var line1 = new Typed(".typed-line1", {
-    strings: ["> lab0324 is a web3 product studio^1000 <br/>> currently building <a href=\"https://www.aikido.work\">www.aikido.work</a>^1000 <br/>> follow us on <a href=\"https://www.twitter.com/lab0324\">twitter</a>"],
-    typeSpeed: 50,
-    backSpeed: 30,
+    strings: [">^3000 lab0324 is a multidisciplinary product studio^3000<br/>> updates: <a href=\"https://www.twitter.com/lab0324\">twitter</a>"],
+    typeSpeed: 40,
+    backSpeed: 0,
     backDelay: 0,
     startDelay: 0,
     loop: false,
